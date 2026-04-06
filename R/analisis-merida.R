14435fe66037790695681a8137d6cccd77cdd14a
erickbatzzz <erickmeza9494@gmail.com>
2026-04-05 22:21:34 -0600

    feat(funcion): añadimos función para cargar datos limpios al script de analisis

--- R/analisis-merida.R
+++ R/analisis-merida.R
@@ -12,10 +12,41 @@ p_load(here, tidyverse, janitor, wesanderson, jsonlite, httr2)
 
 # ---- Generamos temas predeterminados para gráficas ----
 theme_regular <- function() {
-  theme(
-    
+  theme(plot.title = element_text(size = 15,
+                                  hjust = 0.5),
+        plot.subtitle = element_text(size = 9,
+                                     hjust = 0.5),
+        plot.title.position = "plot",
+        plot.caption = element_text(size = 9,
+                                    hjust = 1),
+        plot.caption.position = "plot",
+        legend.title = element_text(size = 11)
   )
 }
+
+# ---- Cargamos nuestros datos usando una función ----
+datos <- list("datos_denue" = "DENUE_viv_yuca",
+              "nada" = "nada")
+
+cargar_datos <- function(datos) {
+  for (nombre in names(datos)){
+    
+    archivo <- datos[[nombre]]
+    ruta <- here("datos",paste0(archivo,".rds"))
+    
+    if(file.exists(ruta)) {
+      message(paste("cargando", archivo, "como objeto: ", nombre))
+      data <- read_rds(ruta)
+      assign(nombre, data, envir = .GlobalEnv)
+    } else {
+      print(paste("El archivo: ", archivo, "no existe"))
+    }
+  }
+}
+
+cargar_datos(datos)
+
+
 
 
 
