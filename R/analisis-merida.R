4994fd962e52a299fd0f4aa26d3e48147692326e
erickbatzzz <erickmeza9494@gmail.com>
2026-05-13 23:12:38 -0600

    chore(refactor): reorganize project directory structure

--- R/analisis-merida.R
+++ R/analisis-merida.R
@@ -5,46 +5,23 @@
 #           y datos abiertos del Sistema Nacional de Indicadores de Vivienda
 # ============================================================================
 
-# Cargamos librerías
+
+# Carga de librerías ------------------------------------------------------
 
 library(pacman)
-p_load(here, tidyverse, janitor, wesanderson, jsonlite, httr2)
+p_load(here, tidyverse, janitor, wesanderson, skimr)
 
-# ---- Generamos temas predeterminados para gráficas ----
-theme_regular <- function() {
-  theme(plot.title = element_text(size = 15,
-                                  hjust = 0.5),
-        plot.subtitle = element_text(size = 9,
-                                     hjust = 0.5),
-        plot.title.position = "plot",
-        plot.caption = element_text(size = 9,
-                                    hjust = 1),
-        plot.caption.position = "plot",
-        legend.title = element_text(size = 11)
-  )
-}
 
-# ---- Cargamos nuestros datos usando una función ----
-datos <- list("datos_denue" = "DENUE_viv_yuca",
-              "nada" = "nada")
-
-cargar_datos <- function(datos) {
-  for (nombre in names(datos)){
-    
-    archivo <- datos[[nombre]]
-    ruta <- here("datos",paste0(archivo,".rds"))
-    
-    if(file.exists(ruta)) {
-      message(paste("cargando", archivo, "como objeto: ", nombre))
-      data <- readRDS(ruta)
-      assign(nombre, data, envir = .GlobalEnv)
-    } else {
-      print(paste("El archivo: ", archivo, "no existe"))
-    }
-  }
-}
+# Carga de datos ----------------------------------------------------------
+# Correr antes: utils_general.R 
 
 cargar_datos(datos)
+
+
+# inspección de la base del DENUE ----------------------------------------
+
+skim(datos_denue)
+view(count(datos_denue, ))
 
 
 
